6167a5fb075ef1ff19153131a58e71c11a8a17da
MA1zaxaka1996 <alexracovets@gmail.com>
2024-12-11 14:29:21 +0200

    fixed pda

--- components/shared/pDA.tsx
+++ components/shared/pDA.tsx
@@ -9,30 +9,42 @@ import usePDA from '@/store/usePDA';
 export const PDA = () => {
     const videoRef = useRef<HTMLVideoElement>(null);
     const isPlay = usePDA(state => state.isPlay);
+    const setIsPlay = usePDA(state => state.setIsPlay);
+
     const [isShow, setIsShow] = useState(false);
 
-    const handlePlay = () => {
+    const handleVideoPlay = () => {
         if (videoRef.current) {
-            videoRef.current.play();
+            videoRef.current.currentTime = 0;
+            setTimeout(() => {
+                videoRef?.current?.play();
+            }, 300);
         }
+    };
+
+    const handleVideoEnd = () => {
+        setTimeout(() => {
+            setIsPlay(false);
+        }, 300);
     };
 
     useEffect(() => {
         setIsShow(isPlay);
-        if (isPlay) handlePlay();
+        if (isPlay) handleVideoPlay();
     }, [isPlay]);
 
     return (
         <div
             className={cn(
-                "absolute left-0 top-0 w-full h-screen flex items-center justify-center bg-black cursor-pointer z-[-1]",
-                isShow ? "z-1" : "z-[-1]"
+                "absolute left-0 top-0 w-full h-screen flex items-center justify-center bg-black z-[-1]",
+                isShow ? "z-1 opacity-1" : "z-[-1] opacity-0",
+                "transition-opacity duration-300 ease-in-out"
             )}
         >
             <video
                 ref={videoRef}
-                style={{ height: "100%", width: "100%", objectFit: "cover" }}
-                muted
+                onEnded={handleVideoEnd}
+                className="w-full h-full object-cover"
             >
                 <source src="/pda/pda_open.mp4" type="video/mp4" />
             </video>
