11f386d51933e32d47cad14bdedf867ea572eca4
MA1zaxaka1996 <alexracovets@gmail.com>
2024-12-11 21:39:03 +0200

    test

--- components/shared/pDAVideo.tsx
+++ components/shared/pDAVideo.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
+import { AnimatePresence, motion } from "framer-motion";
 
 import { cn } from "@/lib/utils";
 import usePDA from '@/store/usePDA';
@@ -14,14 +15,10 @@ export const PDAVideo = () => {
 
     const setIsPlay = usePDA(state => state.setIsPlay);
 
-    const [isShow, setIsShow] = useState(false);
-
     const handleVideoPlay = () => {
         if (videoRef.current) {
             videoRef.current.currentTime = 0;
-            setTimeout(() => {
-                videoRef?.current?.play();
-            }, 300);
+            videoRef.current.play();
         }
     };
 
@@ -31,32 +28,34 @@ export const PDAVideo = () => {
         } else {
             router.push("/");
         }
-        
         setTimeout(() => {
             setIsPlay(false);
-        }, 300);
+        }, 300)
+
     };
 
     useEffect(() => {
-        setIsShow(isPlay);
         if (isPlay) handleVideoPlay();
     }, [isPlay]);
 
     return (
-        <div
-            className={cn(
-                "absolute left-0 top-0 w-full h-screen flex items-center justify-center bg-black z-[-1]",
-                isShow ? "z-1 opacity-1" : "z-[-1] opacity-0",
-                "transition-opacity duration-300 ease-in-out"
-            )}
-        >
-            <video
-                ref={videoRef}
-                onEnded={handleVideoEnd}
-                className="w-full h-full object-cover"
-            >
-                <source src="/pda/pda_open.mp4" type="video/mp4" />
-            </video>
-        </div>
+        <AnimatePresence>
+            {
+                isPlay &&
+                <motion.dialog
+                    className={cn(
+                        "absolute left-0 top-0 w-full h-screen flex items-center justify-center bg-black"
+                    )}
+                >
+                    <video
+                        ref={videoRef}
+                        onEnded={handleVideoEnd}
+                        className="w-full h-full object-cover"
+                    >
+                        <source src="/pda/pda_open.mp4" type="video/mp4" />
+                    </video>
+                </motion.dialog>
+            }
+        </AnimatePresence>
     );
 };
