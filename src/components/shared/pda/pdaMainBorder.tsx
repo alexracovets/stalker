05d9abec87bb7a2c388abd832a04fd2ce61e2917
happy436 <lelouch436@gmail.com>
2025-01-08 14:33:33 +0200

    маппинг картинок

--- src/components/shared/pda/pdaMainBorder.tsx
+++ src/components/shared/pda/pdaMainBorder.tsx
@@ -3,31 +3,58 @@
 import Image from "next/image";
 
 export const PDAMainBorder = () => {
+	// Массив с данными для каждого края
+	const borders = [
+		{
+			position: "top",
+			src: "/pda/border/top.jpg",
+			height: "1rem",
+			width: "100%",
+		},
+		{
+			position: "bottom",
+			src: "/pda/border/horizontal_center.jpg",
+			height: ".8rem",
+			width: "100%",
+		},
+		{
+			position: "left",
+			src: "/pda/border/left.jpg",
+			height: "100%",
+			width: ".9rem",
+		},
+		{
+			position: "right",
+			src: "/pda/border/right.jpg",
+			height: "100%",
+			width: ".9rem",
+		},
+	];
 
-    return (
-        <div className="absolute left-0 top-0 w-full h-full z-[-1]">
-            <div className="relative w-full h-full">
-                <div className="absolute left-0 top-0 w-full h-[1rem] z-[1]">
-                    <div className="relative w-full h-full">
-                        <Image src="/pda/border/top.jpg" fill alt="top" className="object-cover" />
-                    </div>
-                </div>
-                <div className="absolute left-0 bottom-0 w-full h-[.8rem]">
-                    <div className="relative w-full h-full">
-                        <Image src="/pda/border/horizontal_center.jpg" fill alt="bottom" className="object-cover" />
-                    </div>
-                </div>
-                <div className="absolute left-0 top-0 w-[.9rem] h-full">
-                    <div className="relative w-full h-full">
-                        <Image src="/pda/border/left.jpg" fill alt="left" className="object-cover" />
-                    </div>
-                </div>
-                <div className="absolute right-0 top-0 w-[.9rem] h-full">
-                    <div className="relative w-full h-full">
-                        <Image src="/pda/border/right.jpg" fill alt="right" className="object-cover" />
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="absolute left-0 top-0 w-full h-full z-[-1]">
+			<div className="relative w-full h-full">
+				{borders.map(({ position, src, height, width }, index) => {
+					const isVertical =
+						position === "left" || position === "right";
+					const styles = isVertical
+						? `absolute ${position}-0 top-0 w-[${width}] h-full`
+						: `absolute left-0 ${position}-0 w-full h-[${height}]`;
+
+					return (
+						<div key={index} className={styles}>
+							<div className="relative w-full h-full">
+								<Image
+									src={src}
+									fill
+									alt={position}
+									className="object-cover"
+								/>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
